0c5f031a8dab360d1f89ea6276d8d874f661c817
Kien01102004 <Kien01.10.2004@gmail.com>
2026-06-30 14:07:53 +0700

    update login by data exist in database

---
+++ BE/src/main/java/base/api/shared/entity/RoleModel.java
@@ -0,0 +1,23 @@
+package base.api.shared.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class RoleModel {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+}
